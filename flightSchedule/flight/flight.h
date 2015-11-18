94a7f5d18b005d09a220650dd41900909fdcd782
Yohannes <personalprojectyohannes@gmail.com>
2015-11-17 23:14:09 -0600

    [FEATURE] instantiate data Flight class

--- flightSchedule/flight/flight.h
+++ flightSchedule/flight/flight.h
@@ -10,6 +10,16 @@ using namespace std;
 class Flight
 {
 	public:
+		Flight(		
+			int id, 
+			int flightNumber, 
+			string origin, 
+			string destination,
+			string departureDate,
+			string departureTime,
+			string returnDate,
+			string returnTime
+		);
 		int getId();
 		int getFlightNumber();
 		string getOrigin(); 
@@ -20,15 +30,15 @@ class Flight
 		string getReturnTime();
 		vector<int> getPassengerIds();
 		vector<string> getPassengerNames();
-		int setFlightNumber();
-		string setOrigin(); 
-		string setDestination(); 
-		string setDepartureDate(); 
-		string setDepartureTime();
-		string setReturnDate(); 
-		string setReturnTime();
-		vector<int> setPassengerIds();
-		vector<string> setPassengerNames();
+		void setFlightNumber(int flightNumber);
+		void setOrigin(string origin); 
+		void setDestination(string destination); 
+		void setDepartureDate(string departureDate); 
+		void setDepartureTime(string departureTime);
+		void setReturnDate(string returnDate); 
+		void setReturnTime(string returnTime);
+		void addPassengerId(int passengerId);
+		void addPassengerName(string passengerName);
 	private:
 		int id;
 		int flightNumber;
@@ -40,4 +50,4 @@ class Flight
 		string returnTime;
 		vector<int> passengerIds;
 		vector<string> passengerNames;
-}
+};
