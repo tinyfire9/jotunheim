aa116edc9f01861aa3227962d59ed3c829d6a5c4
chea1tei <teikhon@gmail.com>
2015-11-18 20:36:08 -0600

    [TASK]Modified plane headers

--- fleet/plane/plane.h
+++ fleet/plane/plane.h
@@ -17,18 +17,36 @@ class Plane
 {
 public:
     
-    int get_plane_id(int id);
-    int get_column(int column);
-    int get_number_of_economy_class_rows(int economy_class_rows);
-    int get_number_of_economy_plus_rows(int economy_plus_rows);
-    int get_number_of_first_class_rows(int first_class_rows);
+    Plane(
+            int plane_id,
+            int column,
+            int number_of_economy_class_rows,
+            int number_Of_economy_plus_rows,
+            int number_of_first_class_rows
+            
+          );   
+      
+    int get_plane_id ();
+   
+    int get_column();
+
+    int get_number_of_economy_class_rows();
+
+    int get_number_of_economy_plus_rows();
+
+    int get_number_of_first_class_rows();
+
     
- 
+    
     
     void set_column(int column);
+    
     void set_number_of_economy_class_rows(const int rows);
+    
     void set_number_of_economy_plus_rows(const int rows);
+    
     void set_number_of_first_class_rows(const int rows);
+    
     
 private:
     int plane_id;
@@ -36,6 +54,7 @@ private:
     int number_of_economy_class_rows;
     int number_of_economy_plus_rows;
     int number_of_first_class_rows;
+    
     
    
 };
