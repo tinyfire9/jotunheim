b57dae2e67c9b40a266e5edb60a7d418e178b8e3
Yohannes <personalprojectyohannes@gmail.com>
2015-11-23 22:24:27 -0600

    [FEATURE] add split on FlightScheduleUtility// [FEATURE] readline and split by pipes on populateArray

--- utility/flightSchedule/flightScheduleUtility.cpp
+++ utility/flightSchedule/flightScheduleUtility.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <fstream>
 #include "flightScheduleUtility.h"
 
@@ -15,10 +16,33 @@ int FlightScheduleUtility::generateId(vector<Flight> &flights){
 	return maximum;
 }
 
-void FlightScheduleUtility::populateReadArray(vector<Flight> &flights, string fileName){
-	string data = "";
+void FlightScheduleUtility::populateReadArray(/*vector<Flight> &flights, string fileName*/){
+	string line = "";
+	vector<string> data;
+	vector<string> info;
+	vector<string> passengers;
+	FlightScheduleUtility util;
 	fstream readStream;
+
 	readStream.open("../data/flightSchedule.txt");
-	getline(readStream, data);
-	// cout << "----> " << data << endl;
+	while(getline(readStream, line, '|'))
+	{
+		data.push_back(line);
+	}
+	util.split(data[0], info);
+	for(int i = 1; i < data.size(); i++)
+	{
+		util.split(data[i], passengers);
+	}
+
+}
+
+void FlightScheduleUtility::split(string line, vector<string> &words)
+{
+	stringstream ss(line);
+	string word = "";
+	while(ss >> word)
+	{
+		words.push_back(word);
+	}
 }
