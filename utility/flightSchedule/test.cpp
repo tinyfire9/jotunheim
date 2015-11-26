2d6c67aa641050a0f6cc11ff9466200b5e811678
Yohannes <personalprojectyohannes@gmail.com>
2015-11-26 06:12:10 -0600

    [TEST] test method populateReadArray

--- utility/flightSchedule/test.cpp
+++ utility/flightSchedule/test.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
-#include <algorithm>
+// #include ""
 #include "flightScheduleUtility.cpp"
 
 int main(){
-	
+	std::vector<StorageFlight> flights;
 	FlightScheduleUtility test;
-	test.populateReadArray();
-	
+	test.populateReadArray(flights, "asa");
+	for (int i = 0; i < flights.size(); i++)
+	{
+		cout << flights[i].getFlightNumber() << " " << flights[i].getPlaneId() << " " << flights[i].getOrigin() << " " << flights[i].getDestination() << endl;
+	}
 	return 0;
 }
