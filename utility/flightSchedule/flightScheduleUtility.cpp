0cf19381620fb31bc3661250e416ab51fe515d5c
Yohannes <personalprojectyohannes@gmail.com>
2015-11-25 20:50:53 -0600

    [FEATURE] add split method on flight utility, store data into smaller chunks of  vectors

--- utility/flightSchedule/flightScheduleUtility.cpp
+++ utility/flightSchedule/flightScheduleUtility.cpp
@@ -16,14 +16,16 @@ int FlightScheduleUtility::generateId(vector<Flight> &flights){
 	return maximum;
 }
 
-void FlightScheduleUtility::populateReadArray()
+void FlightScheduleUtility::populateReadArray(vector<Flight> &flights, string filename)
 {
+	FlightScheduleUtility util;
 	string line = "";
 	string chunk = "";
 	vector< vector<string> > data;
 	fstream readStream;
 	readStream.open("../data/flightSchedule.txt");
 
+	//read each line and split by |
 	while(getline(readStream, line, '\n'))
 	{
 		vector<string> row;
@@ -33,5 +35,30 @@ void FlightScheduleUtility::populateReadArray()
 			row.push_back(chunk);
 		}
 		data.push_back(row);
+	}
+	for (int i = 0; i < data.size(); i++)
+	{
+		vector<string> chunks;
+		vector< vector<string> > passsenger;
+		util.split(data[i][0], chunks);
+		for (int j = 1; j < ; j++)
+		{
+			vector<string> passsengerInfo;
+			util.split(data[i][j], passsengerInfo);
+			passsenger.push_back(passsengerInfo);
+		}
+		
+		// @TODO
+		// structure the data using flight classes
+	}
+}
+
+void FlightScheduleUtility::split(string line, vector<string> &words)
+{
+	stringstream ss(line);
+	string word = "";
+	while(ss >> word)
+	{
+		words.push_back(word);
 	}
 }
