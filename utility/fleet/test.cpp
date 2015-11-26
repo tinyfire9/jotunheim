5135e45b8380cc38d80c595657c3f33de2ef4923
chea1tei <teikhon@gmail.com>
2015-11-26 00:50:52 -0600

    [TEST]test method for populateReadArray

---
+++ utility/fleet/test.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "utilityPlane.cpp"
+
+int main() {
+
+
+
+PlaneUtility a;
+
+
+
+
+a.populateReadArray();
+
+
+
+    
+
+/*
+
+    stringstream ss;
+	string inp, w;
+	int num;
+	char ch;
+
+	while (cin >> inp) {
+		inp.resize (inp.size () - 1);
+		ss.clear ();
+		ss.str ("");
+		ss << inp;
+		ss >> ch >> num >> ch >> w;
+		cout << num << " <--> " << w << endl;
+	}
+
+	*/
+      
+	return 0;
+}
