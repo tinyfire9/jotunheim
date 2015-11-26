0075d80e35d5e10ec8efc44eb388d48c9be4b4ec
Yohannes <personalprojectyohannes@gmail.com>
2015-11-26 06:14:50 -0600

    [FEATURE] add subclass definition on Flight to use Flight class as a data storage on runtime

---
+++ flightSchedule/flight/storageFlight.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include "flight.cpp"
+using namespace std;
+
+#ifndef STORAGE_FLIGHT_H
+#define STORAGE_FLIGHT_H
+
+class StorageFlight : public Flight{
+public:
+	StorageFlight(int flightNumber, int planeId, string origin, string destination,string departureDate,string departureTime,string returnDate,string returnTime)
+	: Flight(planeId, origin, destination, departureDate, departureTime, returnDate, returnTime){
+		StorageFlight::flightNumber = flightNumber;
+	}
+	int getFlightNumber();
+private:
+	int flightNumber;
+};				
+
+#endif
