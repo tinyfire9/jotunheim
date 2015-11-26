07fb6cf2d1c9d7df1c1222a9af128c94114fcb32
Yohannes <personalprojectyohannes@gmail.com>
2015-11-26 06:08:57 -0600

    [FEATURE] add stringToInt coverter on flight schedule utility // [FEATURE] store all data into a StorageFlight class on populateReadArray// [DOC] add basic comment on populateReadArray

--- utility/flightSchedule/flightScheduleUtility.cpp
+++ utility/flightSchedule/flightScheduleUtility.cpp
@@ -2,21 +2,31 @@
 #include <string>
 #include <sstream>
 #include <fstream>
+#include <stdlib.h>
+#include <typeinfo>
+#include "../../flightSchedule/flight/storageFlight.cpp"
 #include "flightScheduleUtility.h"
 
-int FlightScheduleUtility::generateId(vector<Flight> &flights){
+using namespace std;
+// FlightScheduleUtility util;
+
+int FlightScheduleUtility::generateId(/*vector<Flight> &flights*/){
+	vector<StorageFlight> flights;
+	FlightScheduleUtility util;
+	util.populateReadArray(flights, "flightSchedule.txt");
+
 	int maximum = 0;
 	for(int i = 0; i < flights.size(); i++)
 	{
-		if(flights[i].getId() > maximum)
+		if(flights[i].getFlightNumber() > maximum)
 		{
-			maximum = flights[i].getId();
+			maximum = flights[i].getFlightNumber();
 		}
 	}
 	return maximum;
 }
 
-void FlightScheduleUtility::populateReadArray(vector<Flight> &flights, string filename)
+void FlightScheduleUtility::populateReadArray(vector<StorageFlight> &flights, string filename)
 {
 	FlightScheduleUtility util;
 	string line = "";
@@ -36,20 +46,36 @@ void FlightScheduleUtility::populateReadArray(vector<Flight> &flights, string fi
 		}
 		data.push_back(row);
 	}
+	//(Format) data[i][0] : flightID planeID origin destination depDate depTime retDate retTime
+	// on each iteration, 
+		// breakdown the flight info part of the data ^ by space & put it in a vector
+		// breakdown the passegerInfos (['passID fName lName', 'passID fName lName'] ) by space 
+		//		 and push each data on passenger vector
+
 	for (int i = 0; i < data.size(); i++)
 	{
 		vector<string> chunks;
 		vector< vector<string> > passsenger;
 		util.split(data[i][0], chunks);
-		for (int j = 1; j < ; j++)
+
+		StorageFlight flight(
+			util.stringToInt(chunks[0]), 
+			util.stringToInt(chunks[1]), 
+			chunks[2], 
+			chunks[3],
+			chunks[4], 
+			chunks[5], 
+			chunks[6], 
+			chunks[7]
+		);
+		for (int j = 1; j < data[i].size(); j++)
 		{
 			vector<string> passsengerInfo;
 			util.split(data[i][j], passsengerInfo);
-			passsenger.push_back(passsengerInfo);
+			flight.addPassengerId(util.stringToInt(passsengerInfo[0]));
+			flight.addPassengerName(passsengerInfo[2] + ", " + passsengerInfo[1]);//lastname, firstname
 		}
-		
-		// @TODO
-		// structure the data using flight classes
+		flights.push_back(flight);
 	}
 }
 
@@ -61,4 +87,12 @@ void FlightScheduleUtility::split(string line, vector<string> &words)
 	{
 		words.push_back(word);
 	}
+}
+
+int FlightScheduleUtility::stringToInt(string word)
+{
+	stringstream ss(word);
+	int intVersion;
+	ss >> intVersion;
+	return intVersion;
 }
