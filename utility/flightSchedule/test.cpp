0e955c85f857bbd60dce0621064d41194a2c04a9
Yohannes <personalprojectyohannes@gmail.com>
2015-11-29 11:27:24 -0600

    [TEST] test metods on flight schedule utility

--- utility/flightSchedule/test.cpp
+++ utility/flightSchedule/test.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
-// #include "../../flightSchedule/flight/newFlight.cpp"
+#include "../../flightSchedule/flight/newFlight.cpp"
 #include "flightScheduleUtility.cpp"
 
 int main(){
 	std::vector<StorageFlight> storageFlights;
 	std::vector<NewFlight> newFlights;
 	FlightScheduleUtility test;
-	test.populateReadArray(storageFlights, "asa");
-	for (int i = 0; i < storageFlights.size(); i++)
-	{
-		cout << storageFlights[i].getFlightNumber() << " " << storageFlights[i].getPlaneId() << " " << storageFlights[i].getOrigin() << " " << storageFlights[i].getDestination() << endl;
-	}
-	test.writeFile(storageFlights, newFlights, "aasd");
+	test.populateReadArray(storageFlights);
+	NewFlight test2(10, "DFWW", "JFK","nov.10","10:00am","nov12","9:00pm");
+	test2.addPassengerId(1234);
+	test2.addPassengerName("yohannes seifu");
+
+	test2.addPassengerId(5678);
+	test2.addPassengerName("yoho zel");
+	
+	newFlights.push_back(test2);
+	test.writeFile(storageFlights, newFlights);
+
+	NewFlight test3(10, "SFO", "JFK","dec.22","10:00am","dec23","03:00am");
+	test3.addPassengerId(1234);
+	test3.addPassengerName("yohannes seifu");
+
+	test3.addPassengerId(5678);
+	test3.addPassengerName("yoho zel");
+
+	newFlights.push_back(test3);
+	// cout << storageFlights[5].getReturnDate() <<  storageFlights[5].getReturnDate() << endl;
+	test.writeFile(storageFlights, newFlights);
 	return 0;
 }
