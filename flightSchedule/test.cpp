b97945ba928bcba1029c4d9b8d4151bec2412aec
Yohannes <personalprojectyohannes@gmail.com>
2015-11-29 11:34:17 -0600

    [TEST] test addFlight on flightSchedule

---
+++ flightSchedule/test.cpp
@@ -0,0 +1,16 @@
+#include "flightSchedule.cpp"
+
+int main()
+{
+	FlightSchedule schedule;
+
+	schedule.addFlight(
+		123,
+		"BJI",
+		"CHO",
+		"Jan_12",
+		"1:OOPM",
+		"Jan_13",
+		"6:00PM");
+	return 0;
+}
