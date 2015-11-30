27748d30fa6a36eaf710fe996c5765f87ac366b0
chea1tei <teikhon@gmail.com>
2015-11-29 19:40:09 -0600

    [FEATURE] define methods

--- utility/passenger/passengerUtility.h
+++ utility/passenger/passengerUtility.h
@@ -1,4 +1,18 @@
 #include <iostream>
 #include <string>
+#include "../../passengers/passenger/newPassenger.h"
 #include "../utility.h"
 
+#ifndef PASSENGER_UTILITY_H
+#define PASSENGER_UTILITY_H
+
+class PassengerUtility : public utility{
+public:
+	PassengerUtility(){};
+	void writeFile(vector<StoragePassenger> &storagePassengers, vector<NewPassenger> &newPassengers);
+	void populateReadArray(vector<StoragePassenger> &passengers);
+	void split(string line, vector<string> &words);
+	void stringToInt(string word);
+};
+
+#endif
