8abf7ae322d935f7ee32f634bf8a06562f5f947c
chea1tei <teikhon@gmail.com>
2015-12-01 01:23:23 -0600

    [FEATURE] define data members and method and fleet

--- fleet/fleet.h
+++ fleet/fleet.h
@@ -6,7 +6,7 @@ class Fleet
 {
 public:
 	Fleet();
-	void addPlane(int plane_id, int column, string number_of_economy_class_rows, string number_of_economy_plus_rows, string number_of_first_class_rows);
+	void addPlane(int column, string number_of_first_class_rows, string number_of_economy_class_rows, string number_of_economy_plus_rows);
 private:
 	vector<StoragePlane> storagePlanes;
 	vector<NewPlane> newPlanes;
