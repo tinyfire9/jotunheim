8d231e3605600824700f84bb45412f07469c7554
chea1tei <teikhon@gmail.com>
2015-12-01 03:51:30 -0600

    [FEATURE] passenger methods

--- passengers/passenger/passenger.cpp
+++ passengers/passenger/passenger.cpp
@@ -1,55 +1,69 @@
-#include <iostream>
-#include <string>
-#include <vector>
+/* 
+ * File:   passenger.cpp
+ * Author: Silvester
+ *
+ * Created on November 17, 2015, 9:27 PM
+ */
+
+#include <cstdlib>
 #include "passenger.h"
+#include <vector>
+#include <string>
 
-Passenger::Passenger(
-	int passengerId,
-	string firstName, 
-	string lastName
-	
-)
-{
-	Passenger::passengerId = passengerId;
-	Passenger::firstName = firstName;
-	Passenger::lastName = lastName;
+using namespace std;
+
+#ifndef PASSENGER_CPP
+#define PASSENGER_CPP
+
+
+Passenger::Passenger(   
+    string firstName,
+    string lastName
+   
+){
+ 
+Passenger::firstName = firstName;
+Passenger::lastName = lastName;
+    
 }
 
-int Passenger::getPassengerId(){
-	return Passenger::passengerId;
-}
 
 string Passenger::getFirstName(){
-	return Passenger::firstName;
+    return Passenger::firstName;
 }
 
 string Passenger::getLastName(){
-	return Passenger::lastName;
-}
-
-vector<string> Passenger::getPassengerSeats(){
-	return Passenger::passengerSeats;
+    return Passenger::lastName;
 }
 
 vector<int> Passenger::getFlightNumber(){
-	return Passenger::flightNumbers;
-}
-void Passenger::setPassengerId(int passengerId){
-	Passenger::passengerId =  passengerId;
+    return Passenger:: flightNumbers;
 }
 
-void Passenger::setFirstName(string firstName){
-	Passenger::firstName = firstName;
+vector<string> Passenger::getPassengerSeats()
+{
+    return Passenger::passengerSeats;
 }
 
-void Passenger::setLastName(string lastName){
-	Passenger::lastName = lastName;
+void Passenger::setFirstName(string firstName)
+{
+    Passenger::firstName = firstName;
 }
 
-void Passenger::addFlightNumber(int flightNumber){
-	Passenger::flightNumbers.push_back(flightNumber);
+void Passenger::setLastName(string lastName)
+{
+    Passenger::lastName = lastName;
 }
 
-void Passenger::addPassengerSeat(string passengerSeat){
-	Passenger::passengerSeats.push_back(passengerSeat);
+
+void Passenger::addFlightNumber(int flightNumber)
+{
+    Passenger::flightNumbers.push_back(flightNumber);
 }
+
+void Passenger::addPassengerSeat(string passengerSeat)
+{
+    Passenger::passengerSeats.push_back(passengerSeat);
+}
+
+#endif
