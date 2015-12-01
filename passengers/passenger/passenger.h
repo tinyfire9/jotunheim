5d31818f88d1609d574ff8ae50bc5cd500fbebaf
chea1tei <teikhon@gmail.com>
2015-12-01 03:50:54 -0600

    [TASK] rename id to passengerid and add getter and setter on flight

--- passengers/passenger/passenger.h
+++ passengers/passenger/passenger.h
@@ -1,49 +1,50 @@
+/* 
+ * File:   passenger.h
+ * Author: Silvester
+ *
+ * Created on November 17, 2015, 9:28 PM
+ */
+
+#include <iostream>
 #include <string>
 #include <fstream>
-#include <iostream>
 #include <vector>
-
 #ifndef PASSENGER_H
-#define PASSENGER_H
-
+#define	PASSENGER_H
 using namespace std;
+
 
 class Passenger
 {
-	public:
-		Passenger
-		(
-			int passengerId, 
-			string firstName,
-			string lastName
-		
-		);	
+public:
+    
+    Passenger(
+            string firstName,
+            string lastName     
+          );   
+      
+    string getFirstName();
+    string getLastName();
 
-		int getPassengerId();
-		string getFirstName();
-		string getLastName();
-		
+    vector<int> getFlightNumber();
+    vector<string> getPassengerSeats();  
+ 
+    void setFirstName(string firstName);
+    void setLastName(string lastName);
 
-		vector<int> getFlightNumber();
-		vector<string> getPassengerSeats();
+    void addFlightNumber(int flightNumber);
+    void addPassengerSeat(string passengerSeat);
 
-		void setPassengerId(int passengerId);
-		void setFirstName(string firstName);
-		void setLastName(string lastName);
-		
 
-		void addFlightNumber(int flightNumber);
-		void addPassengerSeat(string passengerSeat);
+private:
 
-		
-
-	private:
-		int passengerId;
-		string firstName;
-		string lastName;	
-		vector<int> flightNumbers;
-		vector<string>passengerSeats;
-		
+    string firstName;
+    string lastName;
+    vector<int> flightNumbers;
+    vector<string> passengerSeats;
+    
+   
 };
 
-#endif
+#endif	
+
