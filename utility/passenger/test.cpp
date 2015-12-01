3d17588c1bd3eb7260738e9c01bacba9430787b9
chea1tei <teikhon@gmail.com>
2015-12-01 03:45:51 -0600

    [TEST] generate new passenger

--- utility/passenger/test.cpp
+++ utility/passenger/test.cpp
@@ -1,15 +1,43 @@
 #include <iostream>
 #include "../../passengers/passenger/newPassenger.cpp"
-#include "passengerUtility.h"
+#include "utilityPassenger.cpp"
 
-int main ()
-{
- 
-   std::vector<StoragePassenger> storagePassengers;
-   std::vector<NewPassenger> newPassengers;
-   PassengerUtility test;
+int main() {
 
-   test.populateReadArray(storagePassengers);
 
- return 0;
+std::vector<StoragePassenger> storagePassengers;
+std::vector<NewPassenger> newPassengers;
+PassengerUtility test;
+
+test.populateReadArray(storagePassengers);
+
+NewPassenger test2("CHEAH","HON");
+test2.addFlightNumber(1234);
+test2.addPassengerSeat("5a");
+
+test2.addFlightNumber(4566);
+test2.addPassengerSeat("8a");
+
+test2.addFlightNumber(8765);
+test2.addPassengerSeat("9a");
+
+newPassengers.push_back(test2);
+test.writeFile(storagePassengers,newPassengers);
+
+NewPassenger test3("cheah","hon");
+test3.addFlightNumber(8999);
+test3.addPassengerSeat("2b");
+
+test3.addFlightNumber(9999);
+test3.addPassengerSeat("5C");
+
+newPassengers.push_back(test3);
+test.writeFile(storagePassengers, newPassengers);
+
+
+
+
+
+return 0;
+	
 }
