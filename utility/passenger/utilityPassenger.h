452283a869908670cbda06b2ca80380d33fd2cbf
chea1tei <teikhon@gmail.com>
2015-12-01 03:37:58 -0600

    [TASK]generate id on passenger instantiation

---
+++ utility/passenger/utilityPassenger.h
@@ -0,0 +1,27 @@
+/* 
+ * File:   utilityPassenger.h
+ * Author: Silvester
+ *
+ * Created on November 19, 2015, 10:01 PM
+ */
+#include "../../passengers/passenger/newPassenger.h"
+#include <string>
+#include <iostream>
+#include "../utility.h"
+ 
+
+#ifndef UTILITYPASSENGER_H
+#define UTILITYPASSENGER_H
+
+class PassengerUtility : public utility{
+
+    public:
+    PassengerUtility(){};
+    void writeFile(vector<StoragePassenger> &storagePassengers,vector<NewPassenger> &NewPassengers);
+    void populateReadArray(vector<StoragePassenger> &passengers);
+    void split(string line, vector<string> &words);
+    int stringToInt(string word);
+    
+};
+
+#endif 
