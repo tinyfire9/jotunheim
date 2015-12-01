fe38c13f2f75bba094a86c57f95c799ecba3ad67
chea1tei <teikhon@gmail.com>
2015-12-01 01:23:43 -0600

    [TASK] remove log

--- fleet/fleet.cpp
+++ fleet/fleet.cpp
@@ -7,14 +7,12 @@ Fleet::Fleet(){
 }
 
 void Fleet::addPlane(
-	int plane_id,
 	int column,
 	string number_of_economy_class_rows,
 	string number_of_economy_plus_rows,
 	string number_of_first_class_rows ){
 	
 	NewPlane plane(
-		plane_id,
 		column,
 		number_of_economy_class_rows,
 		number_of_economy_plus_rows,
@@ -22,4 +20,5 @@ void Fleet::addPlane(
 	);
 	Fleet::newPlanes.push_back(plane);
 	Fleet::utility.writeFile(Fleet::storagePlanes, Fleet::newPlanes);
+    
 }
