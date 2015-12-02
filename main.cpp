7886b53149c7172414d083e3bffeea80c25bbf73
chea1tei <teikhon@gmail.com>
2015-12-02 17:04:05 -0600

    [TEST] main compiler

--- main.cpp
+++ main.cpp
@@ -1,11 +1,29 @@
 
 #include <iostream>
-#include <string>
-#include 
+#include "fleet/fleet.cpp"
+#include "fleet/fleet.h"
+
+
+using namespace std;
+
 
 
 int main()
 {
+	Fleet test;
+
+	test.addPassenger(
+       42,
+       8999,
+       "5a"
+		);
+
+	// PassengerSCH test2;
+
+	// test2.addFlight(
+	// 	2,
+	// 	55,
+	// 	"8A");
 	
-	return 0
-}
+	return 0;
+};
