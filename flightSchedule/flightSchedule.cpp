51d429ffdc88e2487e999a1822b1be08ec406386
Yohannes <personalprojectyohannes@gmail.com>
2015-12-02 09:10:01 -0600

    [FEATURE] add addPassenger method on flightSchedule

--- flightSchedule/flightSchedule.cpp
+++ flightSchedule/flightSchedule.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <algorithm>
 #include "flightSchedule.h"
 #include "./flight/newFlight.cpp"
 
 FlightSchedule::FlightSchedule(){
-	FlightSchedule::utility.populateReadArray(FlightSchedule::storageFlights);
+	FlightSchedule::utility.populateReadArray(FlightSchedule::storageFlights, "../utility/data/flightSchedule.txt");
 }
 
 void FlightSchedule::addFlight(
@@ -25,5 +26,51 @@ void FlightSchedule::addFlight(
 		returnTime
 	);
 	FlightSchedule::newFlights.push_back(flight);
-	FlightSchedule::utility.writeFile(FlightSchedule::storageFlights, FlightSchedule::newFlights);
+	FlightSchedule::utility.writeFile(FlightSchedule::storageFlights, FlightSchedule::newFlights, "../utility/data/flightSchedule.txt");
+}
+
+void FlightSchedule::addPassenger(int flightNumber, int passengerId, string firstName, string lastName){
+	bool found = false;
+	bool duplicatePassenger;
+	for (int i = 0; i < FlightSchedule::storageFlights.size(); i++)
+	{
+		duplicatePassenger = find(
+			FlightSchedule::storageFlights[i].getPassengerIds().begin(), 
+			FlightSchedule::storageFlights[i].getPassengerIds().end(), flightNumber 
+		) != FlightSchedule::storageFlights[i].getPassengerIds().end();
+
+		if(FlightSchedule::storageFlights[i].getFlightNumber() == flightNumber && duplicatePassenger)
+		{
+			FlightSchedule::storageFlights[i].addPassengerId(passengerId);
+			FlightSchedule::storageFlights[i].addPassengerName(lastName + " " + firstName);
+			found = true;
+		}
+	}
+	for (int i = 0; i < newFlights.size(); i++)
+	{
+		duplicatePassenger = find(
+			FlightSchedule::newFlights[i].getPassengerIds().begin(), 
+			FlightSchedule::newFlights[i].getPassengerIds().end(), flightNumber 
+		) != FlightSchedule::newFlights[i].getPassengerIds().end();
+		
+		if(FlightSchedule::newFlights[i].getFlightNumber() == flightNumber && duplicatePassenger)
+		{
+			FlightSchedule::newFlights[i].addPassengerId(passengerId);
+			FlightSchedule::newFlights[i].addPassengerName(lastName + " " + firstName);
+			found = true;
+		}
+	}
+
+	if(!found)
+	{
+		cout << "Entered flight number, " << flightNumber << ", does not exist! Please try again!" << endl;
+	}
+	else if(duplicatePassenger)
+	{
+		cout << "Passenger " << firstName << " " << lastName << ", with Id #" << passengerId << " is already in the flight !" << endl;
+	}
+	else
+	{
+		FlightSchedule::utility.writeFile(FlightSchedule::storageFlights, FlightSchedule::newFlights, "../utility/data/flightSchedule.txt");
+	}
 }
