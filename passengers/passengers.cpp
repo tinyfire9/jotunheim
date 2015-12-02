5d3e4ea71548bd4e4701a353e12eec78d1e0721d
chea1tei <teikhon@gmail.com>
2015-12-02 16:25:24 -0600

    [TASK] require passenger ID as a parameter on adding a passenger

--- passengers/passengers.cpp
+++ passengers/passengers.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include "passengers.h"
 #include "./passenger/newPassenger.cpp"
+#include <algorithm>
 
 PassengerSCH::PassengerSCH(){
-	PassengerSCH::utility.populateReadArray(PassengerSCH::storagePassengers);
+	PassengerSCH::utility.populateReadArray(PassengerSCH::storagePassengers,"../utility/data/passenger.txt");
 }
 
 void PassengerSCH::addPassenger(
@@ -16,5 +17,57 @@ void PassengerSCH::addPassenger(
 		lastName
 	);
 	PassengerSCH::newPassengers.push_back(passenger);
-	PassengerSCH::utility.writeFile(PassengerSCH::storagePassengers, PassengerSCH::newPassengers);
+	PassengerSCH::utility.writeFile(PassengerSCH::storagePassengers, PassengerSCH::newPassengers,"../utility/data/passenger.txt");
+}
+void PassengerSCH::addFlight(int passengerID, int flightNumber, string seat){
+	bool found = false;
+	bool duplicateFlightNumber = false;
+	bool duplicateSeat = false;
+	for (int i = 0; i < PassengerSCH::storagePassengers.size(); i++)
+	{
+		if(PassengerSCH::storagePassengers[i].getPassengerNumber() == passengerID)
+		{
+			found = true;
+			vector<int> ids = PassengerSCH::storagePassengers[i].getFlightNumber();
+			vector<string> seatNumbers = PassengerSCH::storagePassengers[i].getPassengerSeats();
+			for (int j = 0; j < ids.size(); j++)
+			{
+				if(ids[j] == flightNumber)
+				{
+					duplicateFlightNumber = true;
+				}
+			}
+			for (int j = 0; j < seatNumbers.size(); j++)
+			{
+				if(seatNumbers[j] == seat)
+				{
+					duplicateSeat = true;
+				}
+			}
+		}
+		if((duplicateFlightNumber == false) && (duplicateSeat == false) && (found == true))
+		{
+			PassengerSCH::storagePassengers[i].addFlightNumber(flightNumber);
+			PassengerSCH::storagePassengers[i].addPassengerSeat(seat);
+		}
+	}
+
+	
+	if(found == false)
+	{
+		cout << "Entered passenger number, " << passengerID << ", does not exist! Please try again!" << endl;
+	}
+	if(duplicateFlightNumber == true)
+	{
+		cout << "Flight number " << flightNumber << ", with seat number " << seat << " is already taken in the flight !" << endl;
+	}
+	if(duplicateSeat == true)
+	{
+		cout << "Seat number " << seat << ", with passenger Id " << flightNumber << " is already taken in the flight !" << endl;
+	}
+	else
+	{
+		PassengerSCH::utility.writeFile(PassengerSCH::storagePassengers, PassengerSCH::newPassengers, "../utility/data/passenger.txt");
+	}
+    
 }
