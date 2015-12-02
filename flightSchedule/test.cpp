b9ec8d35633b6a9c913987c5b418a20490b13368
Yohannes <personalprojectyohannes@gmail.com>
2015-12-02 09:19:23 -0600

    [TEST] test adding passenger

--- flightSchedule/test.cpp
+++ flightSchedule/test.cpp
@@ -4,13 +4,16 @@ int main()
 {
 	FlightSchedule schedule;
 
-	schedule.addFlight(
-		123,
-		"BJI",
-		"CHO",
-		"Jan_12",
-		"1:OOPM",
-		"Jan_13",
-		"6:00PM");
+	// schedule.addFlight(
+	// 	123,
+	// 	"BJI",
+	// 	"CHO",
+	// 	"Jan_12",
+	// 	"1:OOPM",
+	// 	"Jan_13",
+	// 	"6:00PM");
+	schedule.addPassenger(40, 112233, "slimfire", "seifu");
+	schedule.addPassenger(1000, 112233, "slimfire", "seifu");
+	schedule.addPassenger(40, 112233, "slimfire", "seifu");
 	return 0;
 }
