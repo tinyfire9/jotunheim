665a959fc2cf93f366be3970cca6cb247baefd7a
Yohannes <personalprojectyohannes@gmail.com>
2015-12-02 11:13:08 -0600

    [TEST] test flight and plane methods on FlightSchedule

--- flightSchedule/test.cpp
+++ flightSchedule/test.cpp
@@ -12,8 +12,17 @@ int main()
 	// 	"1:OOPM",
 	// 	"Jan_13",
 	// 	"6:00PM");
-	schedule.addPassenger(40, 112233, "slimfire", "seifu");
-	schedule.addPassenger(1000, 112233, "slimfire", "seifu");
-	schedule.addPassenger(40, 112233, "slimfire", "seifu");
+	
+	// schedule.addPassenger(40, 112233, "slimfire", "seifu");
+	// schedule.addPassenger(1000, 112233, "slimfire", "seifu");
+	// schedule.addPassenger(40, 112233, "slimfire", "seifu");
+
+	int flightNumber = 40;
+	bool a = schedule.flight(flightNumber);
+	cout << "flight exits? -> " << a << endl ; 
+
+	int planeId = 123;
+	bool b = schedule.plane(flightNumber, planeId);
+	cout << "plane matches on a flight? -> " << b << endl ; 
 	return 0;
 }
