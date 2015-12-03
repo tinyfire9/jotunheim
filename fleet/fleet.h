d7e528f386e0f4c82ca287d9e2bc0d32fbaaf8c4
Yohannes <personalprojectyohannes@gmail.com>
2015-12-02 22:06:41 -0600

    [FEATURE] add available seat getters on fleet

--- fleet/fleet.h
+++ fleet/fleet.h
@@ -7,7 +7,11 @@ class Fleet
 public:
 	Fleet();
 	void addPassenger(int planeNumber, int passengerId, string seat);
-	void addPlane(int column, string number_of_first_class_rows, string number_of_economy_class_rows, string number_of_economy_plus_rows);
+	void addPlane(int column, int number_of_first_class_rows, int number_of_economy_class_rows, int number_of_economy_plus_rows);
+    int getAvailableFirstClassSeats(int planeId);
+    int getAvailableEconomyClassSeats(int planeId);
+    int getAvailableEconomyPlusClassSeats(int planeId);
+
 private:
 	vector<StoragePlane> storagePlanes;
 	vector<NewPlane> newPlanes;
