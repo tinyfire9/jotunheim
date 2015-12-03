346598c7a229077ae17f4f95a7385e6fcfe0e572
Yohannes <personalprojectyohannes@gmail.com>
2015-12-03 03:00:51 -0600

    [FEATURE] wrap features from different class, add transferPassenger on Airline

--- airline.cpp
+++ airline.cpp
@@ -4,9 +4,52 @@
 
 Airline::Airline(){};
 
-void Airline::transferPassengers(int flightNumber, int planeId1, int planeId2){
-	// cout << Airline::fleet.getAvailableFirstClassSeats(27) << endl;
-	// Airline::fleet.addPlane(11,22,33,44);
-	Airline::fleet.addPassenger(27, 40, "6D");
-	// Airline::schedule.addPassenger(22, 333666, "slimfire", "seifu");
+void Airline::transferPassengers(int flightNumber, int currentPlaneId, int newPlaneId){
+	Airline::schedule.displayPassengers(1000);
+	if((Airline::schedule.plane(flightNumber, currentPlaneId) == true) 
+		&& ((Airline::schedule.plane(flightNumber, newPlaneId) == true)))
+	{
+		if(
+			(Airline::fleet.getAvailableFirstClassSeats(currentPlaneId) >= Airline::fleet.getAvailableFirstClassSeats(newPlaneId)) &&
+			(Airline::fleet.getAvailableEconomyClassSeats(currentPlaneId) >= Airline::fleet.getAvailableEconomyClassSeats(newPlaneId)) &&
+			(Airline::fleet.getAvailableEconomyPlusClassSeats(currentPlaneId) >= Airline::fleet.getAvailableEconomyPlusClassSeats(newPlaneId))
+			)
+		{
+			cout << "***HeyyyY!!!!" << endl;
+		}
+		else
+		{
+			cout << "*** :((!!!!" << endl;
+		}
+	}
+	else
+	{
+		cout << ":/ :/ :/" << endl;
+	}
+}
+
+void Airline::addNewFlight(int planeId, string origin, string destination,string departureDate,
+		string departureTime, string returnDate, string returnTime){
+	Airline::schedule.addFlight(planeId, origin, destination,departureDate,
+		departureTime, returnDate, returnTime);
+}
+
+void Airline::addNewPassengerToFlight(int flightNumber, int passengerId, string firstName, string lastName){
+	Airline::schedule.addPassenger(flightNumber, passengerId, firstName, lastName);
+}
+
+void Airline::addNewPlane(int column, int number_of_first_class_rows, int number_of_economy_class_rows, int number_of_economy_plus_rows){
+	Airline::fleet.addPlane(column, number_of_first_class_rows, number_of_economy_class_rows, number_of_economy_plus_rows);
+}
+
+void Airline::addNewPassengerToList(string firstName, string lastName){
+	Airline::passengers.addPassenger(firstName, lastName);
+}
+
+void Airline::displayPassengers(int flightNumber){
+	Airline::schedule.displayPassengers(flightNumber);
+}
+
+void Airline::displayUpcomingFlights(){
+	Airline::schedule.displayUpcomingFlights();
 }
