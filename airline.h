540a1cb856545cb1f99e3379271106d238a2c17a
Yohannes <personalprojectyohannes@gmail.com>
2015-12-02 23:25:25 -0600

    [FEATURE] define wrapper methods to add new plane, passenger and flight

--- airline.h
+++ airline.h
@@ -11,7 +11,17 @@ class Airline
 {
 public:
 	Airline();
-	void transferPassengers(int flightNumber, int planeId1, int planeId2);
+	void transferPassengers(int flightNumber, int currentPlaneId, int newPlaneId);
+	void addFlight(	int planeId, 
+		string origin, 
+		string destination,
+		string departureDate,
+		string departureTime,
+		string returnDate,
+		string returnTime);
+	void addNewPlane(int column, int number_of_first_class_rows, int number_of_economy_class_rows, int number_of_economy_plus_rows);
+	void addNewPassengerToFlight(int flightNumber, int passengerId, string firstName, string lastName);
+	void addNewPassengerToList(string firstName, string lastName);
 private:
 	Fleet fleet;
 	PassengerSCH passengers;
