f40ae4b99d61ef0fc02e97832cefc79153afdc8a
chea1tei <teikhon@gmail.com>
2015-12-06 02:39:53 -0600

    [TEST] test case for validity functions

---
+++ utility/client/test.cpp
@@ -0,0 +1,21 @@
+#include "validity.cpp"
+#include <string>
+
+
+using namespace std;
+
+int main()
+{
+
+	Validity valid;
+    //valid.choiceValid(5a);
+
+    valid.stringValid("hk ");
+
+    valid.dateValid(2g,4,2015);
+
+
+
+	return 0;
+
+}
