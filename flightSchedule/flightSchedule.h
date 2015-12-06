adeb9abebbce9a80cd32648d19b7c8c53bdbd298
Yohannes <personalprojectyohannes@gmail.com>
2015-12-05 23:09:44 -0600

    [FEATURE] add change plane on Flight Schedule, remove flight and change plane to flight on flightSchedule

--- flightSchedule/flightSchedule.h
+++ flightSchedule/flightSchedule.h
@@ -8,11 +8,11 @@ public:
 	FlightSchedule();
 	void addFlight(int planeId, string origin, string destination,string departureDate,string departureTime,string returnDate,string returnTime);
 	void addPassenger(int flightNumber, int passengerId, string firstName, string lastName);
-	bool flight(int flightNumber);
-	bool plane(int flightNumber, int planeId);
+	bool flight(int flightNumber, int planeId);
 	void displayPassengers(int flightNumber);
 	void displayUpcomingFlights();
 	int getMaxFlightId();
+	void changePlane(int flightNumber, int currentPlaneId, int newPlaneId);
 private:
 	vector<StorageFlight> storageFlights;
 	vector<NewFlight> newFlights;
