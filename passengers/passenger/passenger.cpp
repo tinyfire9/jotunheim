e1af2bc2061afafdccd976aa19161999707e6af3
chea1tei <teikhon@gmail.com>
2015-12-12 20:11:58 -0600

    [FIX] all documentation

--- passengers/passenger/passenger.cpp
+++ passengers/passenger/passenger.cpp
@@ -29,41 +29,41 @@ Passenger::lastName = lastName;
 
 
 string Passenger::getFirstName(){
-    return Passenger::firstName;
+    return Passenger::firstName; // client first name
 }
 
 string Passenger::getLastName(){
-    return Passenger::lastName;
+    return Passenger::lastName; // client last name
 }
 
 vector<int> Passenger::getFlightNumber(){
-    return Passenger:: flightNumbers;
+    return Passenger:: flightNumbers; // gets the flight number that are stored in the vector
 }
 
 vector<string> Passenger::getPassengerSeats()
 {
-    return Passenger::passengerSeats;
+    return Passenger::passengerSeats; //  gets the passenger seats that are stored in the vector
 }
 
 void Passenger::setFirstName(string firstName)
 {
-    Passenger::firstName = firstName;
+    Passenger::firstName = firstName; // set client first name when adding new passenger
 }
 
 void Passenger::setLastName(string lastName)
 {
-    Passenger::lastName = lastName;
+    Passenger::lastName = lastName; // set client last name when adding new passenger
 }
 
 
 void Passenger::addFlightNumber(int flightNumber)
 {
-    Passenger::flightNumbers.push_back(flightNumber);
+    Passenger::flightNumbers.push_back(flightNumber); // push the flight number into the vector so that the data will be stored
 }
 
 void Passenger::addPassengerSeat(string passengerSeat)
 {
-    Passenger::passengerSeats.push_back(passengerSeat);
+    Passenger::passengerSeats.push_back(passengerSeat); // push the passengerseat into the vector so that the data will be stored
 }
 
 #endif
