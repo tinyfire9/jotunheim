498faa837604c35c7f0287e60289541e978be7a5
chea1tei <teikhon@gmail.com>
2015-12-13 20:14:45 -0600

    [DOC] the methods for plane with documentation

--- fleet/plane/plane.h
+++ fleet/plane/plane.h
@@ -29,29 +29,35 @@ public:
       
    
     int get_column();
-
+     //Post-condition : returns, an integer, #column number
     int get_number_of_economy_class_rows();
-
+     //Post-condition : returns, an integer, #economy class rows 
     int get_number_of_economy_plus_rows();
-
+    //Post-condition : returns, an integer, #economy plus class rows
     int get_number_of_first_class_rows();
-
+    //Post-condition : returns, an integer, #first class rows
     vector<int> getPassengerIds();
-
+    //Post-condition : returns, a vector of integer, a list passengerIds are in the plane
     vector<string> getPassengerSeats();  
-
+    //Post-condition : returns, a vector of strings, a list of the passeger seats 
     void set_column(int column);
-    
+    //Pre-condition : expects, a integer , the new origin of the flight 
+    //Post-condition : sets the origin to the new origin 
     void set_number_of_economy_class_rows(int number_of_economy_class_rows);
-    
+    //Pre-condition : expects, a integer , the new economy class rows of the plane 
+    //Post-condition : sets the economy plus class to the new economy class rows 
     void set_number_of_economy_plus_rows(int number_of_economy_plus_rows);
-    
+    //Pre-condition : expects, a integer , the new economy plus class rows of the plane 
+    //Post-condition : sets the economy class rows to the new economy plus class rows 
     void set_number_of_first_class_rows(int number_of_first_class_rows);
-    
+    //Pre-condition : expects, a integer , the new first class rows of the plane
+    //Post-condition : sets the first class rows to the new first class rows 
     void addPassengerId(int passengerId);
-
+    //Pre-condition : expects, a integer , the passengerID ("4")
+    //Post-condition : adds the passengerID to the current passengers list of the plane
     void addPassengerSeat(string passengerSeat);
-
+    //Pre-condition : expects, a string , the passengerSeat("4A")
+    //Post-condition : adds the passengerSeat to the current passengers list of the plane
     bool deletePassenger(int passengerId, string passengerSeat);
 
     bool deleteLastPassenger();
