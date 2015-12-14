ea7f0ec774acf6708ee69ad2b2bcfecb8211309d
chea1tei <teikhon@gmail.com>
2015-12-13 20:24:15 -0600

    [DOC] all the methods for plane utility with documentation

--- utility/fleet/utilityPlane.h
+++ utility/fleet/utilityPlane.h
@@ -17,30 +17,31 @@
 class PlaneUtility : public utility{
 
     public:
+    //Instansiate class
     PlaneUtility(){};
+    /*
+            Pre-condition : expects a storagePlane type vector,  a newPlane type vector and a file path
+            Post-condition : loops through each item and gets all the data by from each method from both vectors 
+                    and writes it to a file specified by the user
+    */
     void writeFile(vector<StoragePlane> &storagePlanes,vector<NewPlane> &NewPlanes,string path);
+    /*
+            Pre-condition : expects a storagePlane type vector,  and a file path
+            Post-condition : loops through each line of the text fil, creating a storagePlane object with the data
+                    push each object the the vector 
+    */
     void populateReadArray(vector<StoragePlane> &planes,string path);
+            /*
+            Pre-condition : expects a line and a vector 
+            Post-condition : splits the line by space and pushes each chunk into the vector
+     */
     void split(string line, vector<string> &words);
+     /*
+            Pre-condition : expects an integer of type string
+            Post-condition : return the integer version of the string
+    */
     int stringToInt(string word);
     
-/*
-    private:
-    
-    string getSeats;
-    string getFleet; // maybe is string. not sure yet
-    string getStatus;
-    int getFirstClassRows;
-    int getEconomyRows;
-    int getEconomyPlusRows;
-    int getPlaneId; 
-    int getColumn;
-
-    */
-    //void addPlane(string planeId,string status,int column,int firstClassRows,int economyRows,int economyPlusRows); 
-    //void updateData(string seats, int planeId, string fleet);   
-    //string listAvailablePlanes(string status,string fleet);
-    //vector<int> seatData;
-    //vector<string> planeIdData;
   
     
 };
