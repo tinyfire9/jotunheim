97e699ec20b691c68c96b4d5b9b1dbcca8327933
chea1tei <teikhon@gmail.com>
2015-12-13 19:49:35 -0600

    [DOC] methods for passenger with documentation

--- passengers/passenger/passenger.h
+++ passengers/passenger/passenger.h
@@ -24,16 +24,26 @@ public:
           );   
       
     string getFirstName();
+    //Post-condition : returns, a string, the first name of the passenger
     string getLastName();
+    //Post-condition : returns, a string, the last name of the passenger
 
     vector<int> getFlightNumber();
     vector<string> getPassengerSeats();  
  
     void setFirstName(string firstName);
+    //Pre-condition : expects, a string , the new first name of the passenger
+    //Post-condition : sets the first name to the new passenger 
     void setLastName(string lastName);
+    //Pre-condition : expects, a string , the new last name  of the passenger 
+    //Post-condition : sets the last name to the new passenger 
 
     void addFlightNumber(int flightNumber);
+    //Pre-condition : expects, a integer , the flightNumber ('flightNumber')
+    //Post-condition : adds the flightNumber to the current flight list of the passenger
     void addPassengerSeat(string passengerSeat);
+    //Pre-condition : expects, a string , the passengerSeat (passengerSeat)
+    //Post-condition : adds the passengerSeat to the current passengers list of the passenger
 
 
 private:
